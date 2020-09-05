7d0d238e0dbd4ee655fea283b517dbb0077adbd5
Mark McPherson <msm1089@yahoo.co.uk>
2020-09-05 14:08:24 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# mynameis
+
+When run in a Jupyter notebook, simply returns the notebook filename or the full path to the notebook.
+
+## Examples
+
+Get the notebook name:
+
+```python
+import mynameis
+nb_fname = mynameis.what()
+```
+
+Get the full path to the notebook:
+
+```python
+import mynameis
+nb_path = mynameis.what(path=True)
+```
+
+You would think there was already some built-in way to access the current notebook name, but it took many hours
+of searching for a way to do it. As it seems many others did, I tried using Javascript, but the async nature of
+JS meant that it was unreliable. Finally I stumbled on this [post](https://forums.fast.ai/t/jupyter-notebook-enhancements-tips-and-tricks/17064/39)
+Credit for most of the code goes to the author of this post, thanks!
---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), 'r') as f:
+    long_description = f.read()
+
+setup(
+    name='mynameis',
+    version='0.1.0',
+    author='Mark McPherson',
+    author_email='msm1089@yahoo.co.uk',
+    description='Simply returns either notebook filename or the full path to the notebook when run from Jupyter notebook in browser.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    license='MIT',
+    keywords='jupyter notebook filename'.split(),
+    url='https://github.com/msm1089/mynameis',
+    packages=['mynameis'],
+    package_data={},
+    install_requires=[],
+    python_requires='>=3.4, <4',
+    classifiers=[
+        'Operating System :: OS Independent',
+        'Development Status :: 4 - Beta',
+        'Framework :: Jupyter',
+        'Topic :: Utilities',
+        'License :: OSI Approved :: MIT License'
+    ]
+)
