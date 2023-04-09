c9b543fdc0cf1f2acbdacf613d5c9a9a1b874b20
msm1089 <msm1089@yahoo.co.uk>
2023-04-09 12:18:04 +0100

    Update setup.py
    Revert description change + set version to 1.0.0

--- setup.py
+++ setup.py
@@ -6,10 +6,10 @@ long_description = (here / 'README.md').read_text()
 
 setuptools.setup(
     name='ipynbname',
-    version='2023.1.0',
+    version='1.0.0',
     author='Mark McPherson',
     author_email='msm1089@yahoo.co.uk',
-    description='Simply returns either notebook filename or the full path to the notebook when run from Jupyter notebook or Jupter Lab in browser.',
+    description='Simply returns either notebook filename or the full path to the notebook when run from Jupyter notebook in browser.',
     long_description=long_description,
     long_description_content_type='text/markdown',
     license='MIT',
